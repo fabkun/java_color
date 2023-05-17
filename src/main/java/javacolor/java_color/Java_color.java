e8c0cc30b0923424337e60548161c73829fb3b1b
@fabian.tobias.florez.cortez <fabian.tobias.florez.cortez@ericsson.com>
2023-05-16 23:09:28 -0500

    Inicio

---
+++ src/main/java/javacolor/java_color/Java_color.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package javacolor.java_color;
+
+/**
+ *
+ * @author efafolb
+ */
+public class Java_color {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+    }
+}
